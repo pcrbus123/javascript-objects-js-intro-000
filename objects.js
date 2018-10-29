14633cf7ce203e0bf23a994a12bba1bc711eede6
Pnina Breslav <pcrbus123@gmail.com>
2018-10-29 05:48:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ function updatePlaylist(playlist, name, title){
 playlist[name] = title;
 return playlist
 }
+function removeFromPlaylist(playlist, name){
+delete playlist.name;
+return playlist;
+
+}
